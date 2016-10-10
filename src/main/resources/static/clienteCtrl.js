4c69357a1d1c802d53a0eebf6461b0003bcbfaf7
Davi.'' <Davi.''@192.168.25.4>
2016-10-10 00:12:56 -0300

    Atualização do projeto final.

--- src/main/resources/static/clienteCtrl.js
+++ src/main/resources/static/clienteCtrl.js
@@ -1,7 +1,44 @@
 angular.module("vendas").controller("clienteCtrl", function ($scope, $http) {
+		// Cadastrar novo cliente.	
 		$scope.novoCliente = function (cliente) {
-			$http.post("cliente/novo", cliente).success(function (resultadoDoResponse) {
-				$scope.cliente = resultadoDoResponse;
+			$http.post("cliente/novo", cliente).success(function (resultadoDoResponse) {				
+				// Atualizar a lista com o ultimo usuário cadastrado.
+				$('#lista').append(obtemTemplateLista(resultadoDoResponse));
+				limparCampos();
+				alert("Cliente cadastrado com sucesso!");				
 			});
-		};		
+		};
+		
+		// Lista todos os clientes cadastrados.
+		$scope.listarClientes = function (cliente) {
+			$http.post("cliente/listar").success(function (resultadoDoResponse) {
+				carregarTodosProdutos(resultadoDoResponse);
+			});
+		};
+		
+		// Lista todos os cliente ao iniciar a página.
+		$http.post("cliente/listar").success(function (resultadoDoResponse) {
+			carregarTodosProdutos(resultadoDoResponse);
+		});
+});
+
+function carregarTodosProdutos(listaClientes) {
+	$(listaClientes).each(function(index, element){
+		$('#lista').append(obtemTemplateLista(element));				
 	});
+}
+
+function obtemTemplateLista(cliente) {
+	var template = '<div id="coluna" class="col-xs-12 paddinZero">' +
+					   '<label class="col-xs-6">'+ cliente.codigo + '</label>' +
+					   '<label class="col-xs-6">' + cliente.nome + '</label>' +
+				   '</div>';
+	
+	return template;
+}
+
+function limparCampos() {
+	$('input').each(function(index, element){
+		$(element).val('');
+	});
+}
--- src/main/resources/static/produtoCtrl.js
+++ src/main/resources/static/produtoCtrl.js
@@ -1,7 +1,46 @@
 angular.module("vendas").controller("produtoCtrl", function ($scope, $http) {
-		$scope.novoProduto = function (produto) {
-			$http.post("produto/novo", produto).success(function (resultadoDoResponse) {
-				$scope.produto = resultadoDoResponse;
-			});
-		};		
+	// Cadastrar um novo produto.
+	$scope.novoProduto = function (produto) {
+		$http.post("produto/novo", produto).success(function (resultadoDoResponse) {			
+			// Atualizar a lista com o ultimo usuário cadastrado.
+			$('#lista').append(obtemTemplateLista(resultadoDoResponse));
+			limparCampos();
+			alert("Produto cadastrado com sucesso!");
+		});
+	};
+	
+	// listar todos os produtos.
+	$scope.listarTodosProduto = function () {
+		$http.post("produto/listar").success(function (resultadoDoResponse) {			
+			carregarTodosProdutos(resultadoDoResponse);			
+		});
+	};	
+	
+	// Lista todos os produtos ao iniciar a página.
+	$http.post("produto/listar").success(function (resultadoDoResponse) {			
+		carregarTodosProdutos(resultadoDoResponse);			
+	});	
+});
+
+function carregarTodosProdutos(listaProduto) {
+	$(listaProduto).each(function(index, element){
+		$('#lista').append(obtemTemplateLista(element));				
 	});
+}
+
+function obtemTemplateLista(produto) {
+	var template = '<div id="coluna" class="col-xs-12 paddinZero">' +
+					   '<label class="col-xs-3">'+ produto.codigo + '</label>' +
+					   '<label class="col-xs-3">' + produto.nome + '</label>' +
+					   '<label class="col-xs-3">' + produto.tipo +'</label>' +
+					   '<label class="col-xs-3">' + produto.quantidade + '</label>' +
+				   '</div>';
+	
+	return template;
+}
+
+function limparCampos() {
+	$('input').each(function(index, element){
+		$(element).val('');
+	});
+}
